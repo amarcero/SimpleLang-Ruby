89b540276891763a94307da87620c023ea60f2e3
Alan Marcero <alanmarcero@gmail.com>
2014-11-09 11:50:26 -0500

    move controllers to welcome

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,23 @@
-class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+ # Alan Marcero
+
+ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  # variables that will be accessible to all classes
+  # that inherit ApplicationController
+  def initialize
+    @@errors = nil
+    @@lineCount = 1
+    @@IDs = Hash.new
+  end
+
+  # pre:  @@errors must be nil - only 1 error will be pushed
+  # post: the input error & msg are in the @@errors variable
+  #       returns a Hash with :type as the error type
+  #         and :msg as the error message
+  def crash(error, msg)
+    unless @@errors
+      @@errors = Hash[:type => error + " ERROR", :msg => msg]
+      nil # return nil to the system
+    end
+  end
 end
